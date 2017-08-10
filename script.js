5ada2040d0fc98849173ed186e6fcb1f87934106
Hack Reactor Student <student@hackreactor.com>
2017-08-10 11:43:59 -0500

    PIANk

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+console.log("page is loaded")
+
+$("#w1").click(function(){
+$("#w1").css("background-color", "pink");
+})
+
 
 })
